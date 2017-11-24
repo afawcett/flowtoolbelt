4284cc17ca07d587bab59cc38146c1217b27cd4e
Andrew Fawcett <andrewfawcett@Sarahs-MacBook.local>
2017-11-23 18:08:55 -0800

    Added ability to run auto launched flows

--- src/aura/flow/flowController.js
+++ src/aura/flow/flowController.js
@@ -1,5 +1,6 @@
 ({
 	onRecordIdChange : function(component, event, helper) {
+	    component.set("v.body", [ ]);
 	    component.find("recordLoader").reloadRecord();
 	},
     onRecordUpdated : function(component, event, helper) {
@@ -17,36 +18,36 @@
         } else if(changeType == 'LOADED') {
             component.set('v.metadataRecordError', null);
             console.log('Metadata Record Flow ' + component.get('v.metadataRecord.flowtb__Flow__c') );
-            $A.createComponent("lightning:flow", { 'onstatuschange' : component.getReference('c.onFlowStatusChange') }, 
-                function(newFlow, status, errorMessage) {
-                    component.set("v.body", [ newFlow ]);
-                    newFlow.startFlow(
-                        component.get('v.metadataRecord.flowtb__Flow__c'),
-                        [ { name: 'flowtb_record', type : 'SObject', value : component.get('v.simpleRecord') } ] );
-                });                     
+            if(component.get('v.metadataRecord.flowtb__IsAutolaunched__c')) {
+                var action = component.get("c.runFlow");
+                action.setParams({ 
+                    flow : component.get('v.metadataRecord.flowtb__Flow__c'),
+                    flowOutputParamNames : component.get('v.metadataRecord.flowtb__AutoLaunchedOutputVariables__c'),  
+                    record : component.get('v.simpleRecord') });  
+                action.setCallback(this, function(response) {
+                    var state = response.getState();
+                    if (state === "SUCCESS") {
+                        var outputVariables = response.getReturnValue();
+                        helper.handleFlowOutput(component, outputVariables);
+                    }});
+                $A.enqueueAction(action);                
+            } else {
+                $A.createComponent("lightning:flow", { 'onstatuschange' : component.getReference('c.onFlowStatusChange') }, 
+                    function(newFlow, status, errorMessage) {
+                        component.set("v.body", [ newFlow ]);
+                        newFlow.startFlow(
+                            component.get('v.metadataRecord.flowtb__Flow__c'),
+                            [ { name: 'flowtb_record', type : 'SObject', value : component.get('v.simpleRecord') } ] );
+                    });                     
+            }
         }    
     },
     onFlowStatusChange : function(component, event, helper) {
         console.log('Flow status change ' + event.getParam("status"));
         if(event.getParam("status") === "FINISHED" || event.getParam("status") == 'FINISHED_SCREEN') {
-            var eventToFire = null;
-            var eventParams = {};
             var outputVariables = event.getParam("outputVariables");
-            var outputVar;
             if(outputVariables!=null) {
-                for(var i = 0; i < outputVariables.length; i++) {
-                    outputVar = outputVariables[i];
-                    if(outputVar.name === 'flowtb_event') {
-                        eventToFire = $A.get(outputVar.value);
-                    } else if (outputVar.name.startsWith('flowtb_param')) {
-                        var paramName = outputVar.name.split('_')[2];
-                        eventParams[paramName] = outputVar.value; 
-                    }                
-                }
-                if(eventToFire!=null) {
-                    eventToFire.setParams(eventParams);
-                    eventToFire.fire();
-                }
+                helper.handleFlowOutput(component, outputVariables);
             }
         }
     }
--- src/aura/flow/flowHelper.js
+++ src/aura/flow/flowHelper.js
@@ -1,5 +1,20 @@
 ({
-	helperMethod : function() {
-		
+	handleFlowOutput : function(component, outputVariables) {
+        var eventToFire = null;
+        var eventParams = {};
+        var outputVar;	
+        for(var i = 0; i < outputVariables.length; i++) {
+            outputVar = outputVariables[i];
+            if(outputVar.name === 'flowtb_event') {
+                eventToFire = $A.get(outputVar.value);
+            } else if (outputVar.name.startsWith('flowtb_param')) {
+                var paramName = outputVar.name.split('_')[2];
+                eventParams[paramName] = outputVar.value; 
+            }                
+        }
+        if(eventToFire!=null) {
+            eventToFire.setParams(eventParams);
+            eventToFire.fire();
+        }		
 	}
 })
