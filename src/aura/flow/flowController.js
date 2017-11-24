5e30f705c356426264b8c3125c561d490bfe7ced
Andrew Fawcett <andrewfawcett@Sarahs-MacBook.local>
2017-11-23 16:38:55 -0800

    Further enhancements to handle flow responses and route to LEX events

--- src/aura/flow/flowController.js
+++ src/aura/flow/flowController.js
@@ -17,11 +17,37 @@
         } else if(changeType == 'LOADED') {
             component.set('v.metadataRecordError', null);
             console.log('Metadata Record Flow ' + component.get('v.metadataRecord.flowtb__Flow__c') );
-            $A.createComponent("lightning:flow", {}, 
+            $A.createComponent("lightning:flow", { 'onstatuschange' : component.getReference('c.onFlowStatusChange') }, 
                 function(newFlow, status, errorMessage) {
                     component.set("v.body", [ newFlow ]);
-                    newFlow.startFlow(component.get('v.metadataRecord.flowtb__Flow__c'));
+                    newFlow.startFlow(
+                        component.get('v.metadataRecord.flowtb__Flow__c'),
+                        [ { name: 'flowtb_record', type : 'SObject', value : component.get('v.simpleRecord') } ] );
                 });                     
         }    
+    },
+    onFlowStatusChange : function(component, event, helper) {
+        console.log('Flow status change ' + event.getParam("status"));
+        if(event.getParam("status") === "FINISHED" || event.getParam("status") == 'FINISHED_SCREEN') {
+            var eventToFire = null;
+            var eventParams = {};
+            var outputVariables = event.getParam("outputVariables");
+            var outputVar;
+            if(outputVariables!=null) {
+                for(var i = 0; i < outputVariables.length; i++) {
+                    outputVar = outputVariables[i];
+                    if(outputVar.name === 'flowtb_event') {
+                        eventToFire = $A.get(outputVar.value);
+                    } else if (outputVar.name.startsWith('flowtb_param')) {
+                        var paramName = outputVar.name.split('_')[2];
+                        eventParams[paramName] = outputVar.value; 
+                    }                
+                }
+                if(eventToFire!=null) {
+                    eventToFire.setParams(eventParams);
+                    eventToFire.fire();
+                }
+            }
+        }
     }
 })
